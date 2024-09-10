dca8540d86d58ba7defbfb09a5235b3a3cd8cccc
Sharwari Gole <75138060+Sharwari19@users.noreply.github.com>
2024-09-10 11:57:43 +0530

    Add files via upload

---
+++ TodoRedux/src/App.jsx
@@ -0,0 +1,18 @@
+import Todo from './components/Todo'
+import './App.css'
+import AddTodo from './components/AddTodo'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn about Redux Toolkit</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
